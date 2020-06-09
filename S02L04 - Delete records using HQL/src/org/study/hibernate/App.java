f0de5a22e3f502fb7f0a591eb536cf83c0ab6dc8
rijuprakash <rijuprakash@192.168.1.102>
2020-06-09 15:48:10 +0530

    HQL Commit 4

---
+++ S02L04 - Delete records using HQL/src/org/study/hibernate/App.java
@@ -0,0 +1,31 @@
+package org.study.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.study.hibernate.entity.Users;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Users.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			session.beginTransaction();
+			
+			session.createQuery("delete from users where user_id = 3").executeUpdate();
+			
+			session.getTransaction().commit();
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+}
