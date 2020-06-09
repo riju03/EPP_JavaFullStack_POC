90dabd662a8b07cedb93a124d715a86ad725cd05
rijuprakash <rijuprakash@192.168.1.102>
2020-06-09 15:43:25 +0530

    fifth commit

---
+++ S01L05 - Updating a record in Database/src/org/study/hibernate/App.java
@@ -0,0 +1,51 @@
+package org.study.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.study.hibernate.entity.Users;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Users.class)
+				.buildSessionFactory();
+
+	
+
+		Session session = factory.getCurrentSession();
+
+		
+
+		try {
+
+			// Create object of entity class type
+
+			Users user = new Users();
+
+			// start transaction
+
+			session.beginTransaction();
+
+			// Perform operation
+		
+			user = session.get(Users.class, 3);
+			
+			//updating object
+			
+			user.setUsername("Abhay0312");
+
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println(user);
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
